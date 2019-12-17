9b8366d91752b31fdff4161ccdf8b38883ca9363
Humaid Khan <humaidk2@gmail.com>
2019-12-17 21:54:20 +0400

    Add notifications

--- warantee/AppDelegate.swift
+++ warantee/AppDelegate.swift
@@ -12,7 +12,7 @@ import Firebase
 import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
@@ -41,8 +41,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 //            }
 //
 //        }
-        
+        registerForRichNotifications()
         return true
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+           print(response.notification.request.content.userInfo)
+           completionHandler()
+       }
+
+       func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+           completionHandler([.alert, .badge, .sound])
+       }
+    func registerForRichNotifications() {
+
+       UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound]) { (granted:Bool, error:Error?) in
+            if error != nil {
+                print(error?.localizedDescription)
+            }
+            if granted {
+                print("Permission granted")
+            } else {
+                print("Permission not granted")
+            }
+        }
+
+        //actions defination
+        let action1 = UNNotificationAction(identifier: "action1", title: "Action First", options: [.foreground])
+        let action2 = UNNotificationAction(identifier: "action2", title: "Action Second", options: [.foreground])
+
+        let category = UNNotificationCategory(identifier: "actionCategory", actions: [action1,action2], intentIdentifiers: [], options: [])
+
+        UNUserNotificationCenter.current().setNotificationCategories([category])
+
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
